40b13233e59525b2e37b2633b3752bba9cda0530
towercity <matthewjnerger3@gmail.com>
2016-10-09 17:51:04 -0400

    Add footer to template

--- js/template.js
+++ js/template.js
@@ -1,4 +1,5 @@
 var template = {
+  /* Header info */
   'header': {
     'html': {
       'start': '<header class="container"><div class="banner"><nav><ul class="header-links">',
@@ -37,8 +38,46 @@ var template = {
       $('body').prepend(printHTML);
     }
   },
+
+  /* Footer */
+  'footer': {
+    'html': `
+    <footer class="footer">
+      <div class="container">
+        <div class="row footer-row">
+          <div>
+            <a href="http://www.as.miami.edu/" target="_blank">
+              <img src="images/um-arts-and-sciences.jpg" alt="UM Arts and Sciences" class="footer-img">
+            </a>
+          </div>
+          <div class="copyright">
+            <div class="footer-social">
+              <a href="http://www.facebook.com/sinkingcityandcafecito" target=_blank>
+                <i class="fa fa-facebook-square" aria-hidden="true"></i>
+              </a>
+              <a href="http://www.twitter.com/sinkingcity" target=_blank>
+                <i class="fa fa-twitter-square" aria-hidden="true"></i>
+              </a>
+            </div>
+            © 2016 by Sinking City. All Rights Reserved.
+          </div>
+          <div>
+            <a href="http://www.as.miami.edu/english/creativewriting/" target="_blank" class="no-decoration">
+              <div class="cw-mfa">Creating Writing MFA @ UM</div>
+            </a>
+          </div>
+        </div>
+      </div>
+    </footer>`,
+    render: function() {
+      $('body').append(this.html);
+    }
+  },
+
+  /* Render all */
   render: function() {
     this.header.render();
+    this.footer.render();
   }
 };
 
