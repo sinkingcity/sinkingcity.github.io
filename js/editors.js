83fceebb0ecb6f16d68c3275a5272a9e3aa48e57
towercity <matthewjnerger3@gmail.com>
2016-10-31 22:45:40 -0400

    Add editors array

---
+++ js/editors.js
@@ -0,0 +1,37 @@
+var editors = [
+	{
+		"name": "Brittany Lyons",
+		"title": "Managing Editor",
+		"picture": ""
+	},
+	{
+		"name": "Chioma Urama",
+		"title": "Fiction Editor",
+		"picture": "/images/editors/chioma.jpg"
+	},
+	{
+		"name": "Dana De Greff",
+		"title": "Fiction Editor",
+		"picture": "/images/editors/dana.jpg"
+	},
+	{
+		"name": "Jon Lampe",
+		"title": "Poetry Editor",
+		"picture": ""
+	},
+	{
+		"name": "Mario Ariza",
+		"title": "Poetry Editor",
+		"picture": ""
+	},
+	{
+		"name": "Candice Mays",
+		"title": "Art Editor",
+		"picture": "/images/editors/candice.jpg"
+	},
+	{
+		"name": "Matthew Nerger",
+		"title": "Nonfiction Editor/Webmaster",
+		"picture": ""
+	}
+]
